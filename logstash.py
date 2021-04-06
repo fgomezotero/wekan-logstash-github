8aeb78299d9b715ff980d8db7e10d57c6185852f
Franklin Gomez <fgomezotero@gmail.com>
2021-04-06 18:00:28 -0300

    feat: add boardSlug field to json solve #9

--- logstash.py
+++ logstash.py
@@ -7,7 +7,7 @@
 # Author: Florent MONTHEL (fmonthel@flox-arts.net)
 #
 # {"storyPoint": 2.0, "nbComments": 1, "createdBy": "fmonthel", "labels": ["vert", "jaune"], 
-# "members": ["fmonthel", "Olivier"], "id": "7WfoXMKnmbtaEwTnn",
+# "members": ["fmonthel", "Olivier"], "id": "7WfoXMKnmbtaEwTnn", "boardSlug": "test",
 # "archivedAt": "2017-02-19T02:13:24.269Z", "createdAt": "2017-02-19T02:13:24.269Z", "lastModification":
 # "2017-02-19T03:12:13.740Z", "list": "Done", "dailyEvents": 5, "board": "Test", "isArchived": true,
 # "duedAt": "2017-02-19T02:13:24.269Z", "swimlaneTitle": "Swinline Title", "customfieldName1": "value",
@@ -51,8 +51,8 @@ def main():
     cards = getcardsdata()
     try:
         for id in cards:
-            print(json.dumps(cards[id], ensure_ascii=False, sort_keys=True))
-            # calllogstashpipeline(json.dumps(cards[id], ensure_ascii=True, sort_keys=True))
+            # print(json.dumps(cards[id], ensure_ascii=False, sort_keys=True))
+            calllogstashpipeline(json.dumps(cards[id], ensure_ascii=True, sort_keys=True))
     except requests.exceptions.RequestException as e:
         print(e)
     finally:
@@ -202,6 +202,7 @@ def getcardsdata():
             # Get board data
             tmp_board = boards.find_one({"_id": card["boardId"]})
             data[card["_id"]]['board'] = tmp_board['title']
+            data[card["_id"]]['boardSlug'] = tmp_board['title'].lower().replace(' ', '-')
             data[card["_id"]]["boardId"] = tmp_board['_id']
             # Public board or in whitelist => get title of cards ?
             # if tmp_board["permission"] == 'public' or tmp_board["_id"] in whitelistboards:
